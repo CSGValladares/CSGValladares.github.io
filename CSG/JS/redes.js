37fa5a0140faeebb36c4682b9177d06ae14e5eaf
CSG_Valladares_5 <112735939+CSGValladares@users.noreply.github.com>
2023-07-13 16:34:16 -0600

    Initial commit

---
+++ CSG/JS/redes.js
@@ -0,0 +1,33 @@
+//Facebook
+
+function facebook(){
+    alert("Espero y te guste mi contenido <3");
+}
+
+// Wattpad
+
+function Wattpad(){
+    alert("Espero y te gusten mis historias <3");
+}
+
+//Twitter
+
+function Twitter(){
+    alert("¡¡Alerta, Entrada hacia contenido para Adultos!!");
+    alert("Estas a punto de entrar a mi sitio web que contiene algo contenido +18");
+    return confirm("¿Enserio deseas entrar a este sitio? Si es asi eres libre de hacerlo y si no pues puedes cancelar")
+}
+
+//Pinterest
+
+function Pinterest(){
+    alert("Espero te guste mis dibujos <3");
+}
+
+function Instagram(){
+    alert("Espero y te guste mis contenido furro owo");
+}
+
+function Youtube(){
+    alert("Espero y te gusten mis videos todos culos");
+}
